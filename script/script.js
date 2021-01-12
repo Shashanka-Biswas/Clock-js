1db5e9a25ae108b1bf9026ddd9358a9eb40f81c2
Shashanka-Biswas <57141382+Shashanka-Biswas@users.noreply.github.com>
2021-01-12 16:26:35 +0600

    0 houre is 12 houre

--- script/script.js
+++ script/script.js
@@ -2,17 +2,21 @@ console.log("script is connected");
 const seccondsDiv = document.querySelector('.secconds');
 const minuiesDiv = document.querySelector('.minuies');
 const houresDiv = document.querySelector('.houres');
+const ampnDiv = document.querySelector('.ampm');
 
 setInterval(timeD, 1000);
-function timeD(){
+function timeD() {
     const now = new Date;
-    let nowSecconds= now.getSeconds();
+    let nowSecconds = now.getSeconds();
     let nowMinuites = now.getMinutes();
     let nowHoures = now.getHours();
-    nowHoures = nowHoures == nowHoures%12 ? nowHoures : nowHoures%12;
+    let ampm = nowHoures > 12 ? "PM" : "AM";
+    nowHoures = nowHoures == nowHoures % 12 ? nowHoures : nowHoures % 12;
+    nowHoures = nowHoures == 0 ? 12: nowHoures;
     seccondsDiv.innerText = nowSecconds;
     minuiesDiv.innerText = nowMinuites;
     houresDiv.innerText = nowHoures;
+    ampnDiv.innerText = ampm;
     // console.log(nowSecconds);
     // console.log(nowMinuites);
     // console.log(nowHoures);
