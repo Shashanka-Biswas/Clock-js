e68f71f8283c14d91e5c0b4d8617c27b8c922cf0
Shashanka-Biswas <57141382+Shashanka-Biswas@users.noreply.github.com>
2021-01-12 16:10:14 +0600

    12 houre formate

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
 console.log("script is connected");
+const seccondsDiv = document.querySelector('.secconds');
+const minuiesDiv = document.querySelector('.minuies');
+const houresDiv = document.querySelector('.houres');
+
+setInterval(timeD, 1000);
+function timeD(){
+    const now = new Date;
+    let nowSecconds= now.getSeconds();
+    let nowMinuites = now.getMinutes();
+    let nowHoures = now.getHours();
+    nowHoures = nowHoures == nowHoures%12 ? nowHoures : nowHoures%12;
+    seccondsDiv.innerText = nowSecconds;
+    minuiesDiv.innerText = nowMinuites;
+    houresDiv.innerText = nowHoures;
+    // console.log(nowSecconds);
+    // console.log(nowMinuites);
+    // console.log(nowHoures);
+}
+
